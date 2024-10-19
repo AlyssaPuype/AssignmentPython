14044eb4f367abaab8f2ccd6c891dec7d70a3432
alyayee <alyssa.puype@gmail.com>
2024-10-19 16:37:46 +0200

    wrote usercode and datacode

--- runtracker.py
+++ runtracker.py
@@ -1,1 +1,21 @@
-import sqlite3
+import database
+
+app_prompt = """
+
+
+1) Add User.
+2) View All Users.
+3) Find User by name.
+4) Add Run Session
+5) View Run Session
+6) View All Run Sessions by User
+7) Remove Run Session
+8) Exit.
+
+"""
+
+def users():
+	connection = database.connect()
+	database.create_tables(connection)
+
+users()
